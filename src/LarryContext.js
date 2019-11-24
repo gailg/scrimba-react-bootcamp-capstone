4b30800a9c28f2ccb0a84f43fc4bf7cd3eff190f
Gail Gong <gailgongster@gmail.com>
2019-11-23 18:43:27 -0800

    capstone project 05 fetch photos

--- src/LarryContext.js
+++ src/LarryContext.js
@@ -1,11 +1,27 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 const LarryContext = React.createContext();
 
 const LarryContextProvider = ({ children }) => {
-  const [larry, setLarry] = useState("");
+  const [photos, setPhotos] = useState([]);
+  const [loading, setLoading] = useState(false);
+
+  const url =
+    "https://raw.githubusercontent.com/bobziroll/scrimba-react-bootcamp-images/master/images.json";
+
+  useEffect(() => {
+    setLoading(true);
+    fetch(url)
+      .then(response => response.json())
+      .then(data => {
+        setPhotos(data);
+        setLoading(false);
+      });
+  }, []);
 
   return (
-    <LarryContext.Provider value={{ larry }}>{children}</LarryContext.Provider>
+    <LarryContext.Provider value={{ loading, photos }}>
+      {children}
+    </LarryContext.Provider>
   );
 };
 
