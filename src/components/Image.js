fe8f3a32b2387280f1915b68c38a469b96f2f9c0
Gail Gong <gailgongster@gmail.com>
2019-11-23 20:38:46 -0800

    capstone project 07 display icons on hover

--- src/components/Image.js
+++ src/components/Image.js
@@ -1,9 +1,25 @@
-import React from "react";
+import React, { useState } from "react";
 
 const Image = ({ img, className }) => {
+  const [hovered, setHovered] = useState(false);
+
+  const heartIcon = hovered && <i className="ri-heart-fill favorite"></i>;
+  const cartIcon = hovered && <i className="ri-add-circle-line cart"></i>;
   return (
     <div className={`${className} image-container`}>
-      <img src={img.url} className="image-grid" alt="" />
+      <img
+        src={img.url}
+        className="image-grid"
+        alt=""
+        onMouseEnter={() => {
+          setHovered(true);
+        }}
+        onMouseLeave={() => {
+          setHovered(false);
+        }}
+      />
+      {heartIcon}
+      {cartIcon}
     </div>
   );
 };
